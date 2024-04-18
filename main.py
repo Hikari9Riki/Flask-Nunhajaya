8aafd974d14bed43726e4f8b3b6277bee79030e2
Hikari9Riki <himito@Duwe>
2024-04-18 21:45:51 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "this is a secret"
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username: ", validators=[DataRequired()])
+    email = EmailField("Email: ", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username: ", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def home():
@@ -10,13 +25,30 @@ def home():
 def admin():
     return render_template("/admin/admin.html")
 
-@app.route('/admin/login')
-def login():
-    return render_template("/admin/login.html")
+def admin_product():
+    return ("Product")
 
-@app.route('/admin/register')
+@app.route('/admin/login', methods=['GET', 'POST'])
+def login():
+    name = None
+    password = None
+    form = LoginForm()
+    return render_template("/admin/login.html", name = name, password = password, form = form)
+
+@app.route('/admin/register', methods=['GET', 'POST'])
 def register():
-    return render_template("/admin/register.html")
+    name = None
+    email = None
+    password = None
+    form = RegisterForm()
+    if form.validate_on_submit():
+        name = form.username.data
+        email = form.email.data
+        password = form.password.data
+        form.username.data = ''
+        form.email.data = ''
+        form.password.data = ''
+    return render_template("/admin/register.html" , name = name, email = email, password = password, form = form)
 
 @app.errorhandler(404)
 def path_to_error(e):
